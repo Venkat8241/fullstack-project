347c3daea47db7ad5c9025bf1b0d893657273b44
Venkat <144604437+Venkat8241@users.noreply.github.com>
2026-02-24 16:57:16 +0530

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
